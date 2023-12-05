76cda075e61a9d456345d0bc9d422b65ee0b418a
Jon-Michael Gelua <geluajm@gmail.com>
2023-12-05 06:28:32 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,22 +8,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        mellow: "#E8E9A1",
-        coral: "#F76C6C",
-        "bluey-light": "#A8D0E6",
-        bluey: "#374785",
-        "bluey-dark": "#24305E",
-      },
-      fontFamily: {
-        voga: "var(--font-voga-medium)",
-        inter: "var(--font-inter)",
-      },
       letterSpacing: {
         loose: ".7em",
       },
@@ -35,15 +19,21 @@ const config: Config = {
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     themes: [
-      "acid",
       "coffee",
       {
-        mytheme: {
+        darky: {
           primary: "#a991f7",
           secondary: "#f6d860",
           accent: "#37cdbe",
           neutral: "#3d4451",
-          "base-100": "#ffffff",
+          "base-100": "#211720",
+        },
+        blanco: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#ffffff",
+          "base-100": "#3d4451",
         },
       },
     ],
